abd2be922c94ebbf5605e4ecec566082fa06c391
Jason Huang <jasonh3308@gmail.com>
2019-02-20 21:54:02 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+module.exports = {
+  extends: [
+    require.resolve('eslint-config-airbnb-base')
+  ],
+  rules: {
+    // add your custom rules here
+    rules: {
+      'no-param-reassign': ['error', {
+        props: false,
+      }],
+      semi: ['error', 'never'],
+      'import/no-extraneous-dependencies': ['error', {
+        'devDependencies': true,
+        optionalDependencies: ['test/unit/index.js'],
+      }],
+      'indent': 'off',
+      'vue/script-indent': ['error', 2, {
+        'baseIndent': 1,
+        'switchCase': 1
+      }],
+      'import/extensions': ['error', 'never'],
+      'no-console': 'off',
+      'import/prefer-default-export': 'off',
+      'import/no-named-as-default': 0,
+      'class-methods-use-this': ['off'],
+      // allow debugger during development
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+      'no-else-return': 'off',
+      'no-restricted-syntax': ["error", "ForInStatement", "LabeledStatement", "WithStatement"],
+    }
+  }
+}
